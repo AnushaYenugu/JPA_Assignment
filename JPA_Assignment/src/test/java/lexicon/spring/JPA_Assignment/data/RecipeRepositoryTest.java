a6d7eb2cf087caefde54535d64cf93b631b1f27b
Anusha Yenugu <anushayenugu92@gmail.com>
2022-09-26 13:58:22 +0200

    changed setup in test case

--- JPA_Assignment/src/test/java/lexicon/spring/JPA_Assignment/data/RecipeRepositoryTest.java
+++ JPA_Assignment/src/test/java/lexicon/spring/JPA_Assignment/data/RecipeRepositoryTest.java
@@ -16,92 +16,120 @@ class RecipeRepositoryTest {
 
     @Autowired
     RecipeRepository recipeRepository;
+
     @Autowired
-    TestEntityManager testEntityManager;
-    Recipe testRecipe;
+    IngredientRepository ingredientRepository;
+    @Autowired
+    RecipeIngredientRepository recipeIngredientRepository;
+    @Autowired
+    RecipeInstructionRepository recipeInstructionRepository;
+    @Autowired
+    RecipeCategoryRepository recipeCategoryRepository;
 
-    public List<Ingredient> ingredients() {
-        return Arrays.asList(
-                new Ingredient("tomato"),
-                new Ingredient("tumeric spice"),
-                new Ingredient("washed peas"),
-                new Ingredient("rice"),
-                //Palak Paneer Ingredients
-                new Ingredient("Spinach") ,
-                new Ingredient("Paneer"));
-    }
-
-
-
-    public Set<RecipeCategory> categories(){
-
-        HashSet<RecipeCategory> result=new HashSet<RecipeCategory>();
-        RecipeCategory vegan =new RecipeCategory("vegan");
-        RecipeCategory vegetarian=new RecipeCategory("vegetarian");
-         result.add(vegan);
-         result.add(vegetarian);
-
-        return result;
-
-    }
 
     @BeforeEach
     void setUp() {
-/**
-        List<Ingredient> persistedIngredients = ingredients().stream().map(testEntityManager::persist).collect(Collectors.toList());
+        Ingredient tomato=new Ingredient("tomato");
+        Ingredient turmeric=new Ingredient("tumeric spice");
+        Ingredient peas=new Ingredient("washed peas");
+        Ingredient rice=new Ingredient("rice");
+        //Palak Paneer Ingredients
+        Ingredient palak=new Ingredient("Spinach");
+        Ingredient  paneer=new Ingredient("Paneer");
 
-        List<RecipeIngredient> recipeIngredients = new ArrayList<>(
-                Arrays.asList(new RecipeIngredient(300, Measurement.G, persistedIngredients.get(3)),
-                        new RecipeIngredient(250, Measurement.G, persistedIngredients.get(0)),
-                        new RecipeIngredient(1, Measurement.TSP, persistedIngredients.get(1)),
-                        new RecipeIngredient(100, Measurement.G, persistedIngredients.get(2))));
+        ingredientRepository.save(tomato);
+        ingredientRepository.save(turmeric);
+        ingredientRepository.save(peas);
+        ingredientRepository.save(rice);
+
+        ingredientRepository.save(palak);
+        ingredientRepository.save(paneer);
+
+        System.out.println(ingredientRepository.findIngredientByIngredientNameIgnoreCase("tomato"));
+        ingredientRepository.findIngredientsByIngredientNameContainsIgnoreCase("t").forEach(System.out::println);
+
+        List<RecipeIngredient> tomatoPulavIngredients=new ArrayList<>();
+        // Recipe tomatoPulavRecipe=new Recipe("Tomato Pulav");
+        //Tomata pulav recipe Ingredients Measuremnt
+        RecipeIngredient tomatoPulavRecipeIngredient=new RecipeIngredient(300, Measurement.G);
+        RecipeIngredient tomatoPulavRecipeIngredient2=new RecipeIngredient(250, Measurement.G );
+        RecipeIngredient tomatoPulavRecipeIngredient3=new RecipeIngredient(1, Measurement.TSP );
+        RecipeIngredient tomatoPulavRecipeIngredient4=new RecipeIngredient(100, Measurement.G );
+        tomatoPulavRecipeIngredient.addIngredient(rice);
+        tomatoPulavRecipeIngredient2.addIngredient(tomato);
+        tomatoPulavRecipeIngredient3.addIngredient(turmeric);
+        tomatoPulavRecipeIngredient4.addIngredient(peas);
+
+        //Palak Recipe Ingredients Measurement
+        RecipeIngredient palakPaneerIngredient1=new RecipeIngredient(250,Measurement.G);
+        RecipeIngredient palakPaneerIngredient2=new RecipeIngredient(200,Measurement.G);
+        // RecipeIngredient palakPaneerIngredient3=new RecipeIngredient(100,Measurement.G);
+
+        palakPaneerIngredient1.addIngredient(palak);
+        palakPaneerIngredient2.addIngredient(paneer);
+        //  palakPaneerIngredient2.addIngredient(tomato);
+
+//Saving Recipeingredients for Tomato
 
 
-        List<RecipeIngredient> persistedRecipeIngredients = recipeIngredients.stream().map(testEntityManager::persist).collect(Collectors.toList());
-        RecipeInstruction instruction = new RecipeInstruction("\"Cook rice first and in another pan add oil, salt, and mentioned ingredients,\" +\n" +
-                "                \"and cook tomatoes and peas and add boiled rice\"");
+        recipeIngredientRepository.save(tomatoPulavRecipeIngredient);
+        recipeIngredientRepository.save(tomatoPulavRecipeIngredient2);
+        recipeIngredientRepository.save(tomatoPulavRecipeIngredient3);
+        recipeIngredientRepository.save(tomatoPulavRecipeIngredient4);
 
-        testEntityManager.persist(instruction);
-        Set<RecipeCategory> persistedCategory = categories().stream().map(testEntityManager::persist).collect(Collectors.toSet());
+        //Saving Recipeingredients for Palak
 
-        Recipe recipe=
-                        new Recipe("Tomato Pulav", persistedRecipeIngredients, instruction, persistedCategory
-                                .stream()
-                                .filter(recipeCategory -> "vegan".equalsIgnoreCase(recipeCategory.getCategory()))
-                                .collect(Collectors.toSet()));
+        recipeIngredientRepository.save(palakPaneerIngredient1);
+        recipeIngredientRepository.save(palakPaneerIngredient2);
+        //   recipeIngredientRepository.save(palakPaneerIngredient3);
 
 
+        tomatoPulavIngredients.add(tomatoPulavRecipeIngredient);
+        tomatoPulavIngredients.add(tomatoPulavRecipeIngredient2);
+        tomatoPulavIngredients.add(tomatoPulavRecipeIngredient3);
+        tomatoPulavIngredients.add(tomatoPulavRecipeIngredient4);
 
-      //  List<Recipe> persistedRecipes = recipes.stream().map(testEntityManager::persist).collect(Collectors.toList());
 
-        recipeRepository.save(recipe);
-        testEntityManager.flush();
-**/
+        HashSet<RecipeCategory> recipeCategories=new HashSet<>();
+        RecipeCategory vegan=new RecipeCategory("vegan");
+        RecipeCategory vegitarian=new RecipeCategory("vegitarian");
+        recipeCategoryRepository.save(vegan);
+        recipeCategoryRepository.save(vegitarian);
+        recipeCategories.add(vegan);
+        recipeCategories.add(vegitarian);
 
-        List<Ingredient> persistedIngredients = ingredients().stream().map(testEntityManager::persist).collect(Collectors.toList());
-        List<RecipeIngredient> recipeIngredients = new ArrayList<>(
-                Arrays.asList(new RecipeIngredient(300, Measurement.G, persistedIngredients.get(3)),
-                        new RecipeIngredient(250, Measurement.G, persistedIngredients.get(0)),
-                        new RecipeIngredient(1, Measurement.TSP, persistedIngredients.get(1)),
-                        new RecipeIngredient(100, Measurement.G, persistedIngredients.get(2))));
-        List<RecipeIngredient> persistedRecipeIngredients = recipeIngredients.stream().map(testEntityManager::persist).collect(Collectors.toList());
-        RecipeInstruction instruction = new RecipeInstruction("\"Cook rice first and in another pan add oil, salt, and mentioned ingredients,\" +\n" +
-                "                \"and cook tomatoes and peas and add boiled rice\"");
-        testEntityManager.persist(instruction);
-        Set<RecipeCategory> persistedCategory = categories().stream().map(testEntityManager::persist).collect(Collectors.toSet());
-        List<Recipe> recipes = new ArrayList<>(
-                Arrays.asList(
-                        new Recipe("Tomato Pulav", persistedRecipeIngredients, instruction, persistedCategory
-                                .stream()
-                                .filter(recipeCategory -> "vegan".equalsIgnoreCase(recipeCategory.getCategory()))
-                                .collect(Collectors.toSet()))
-                )
-        );
-        List<Recipe> persistedRecipes = recipes.stream().map(testEntityManager::persist).collect(Collectors.toList());
-        Set<Recipe> found=recipeRepository.findRecipesByRecipeIngredientsIngredientIngredientName("rice");
+        RecipeInstruction tomatoRecipeInstruction=new RecipeInstruction("Cook rice first and in another pan add oil, salt, and mentioned ingredients," +
+                "and cook tomatoes and peas and add boiled rice");
+        recipeInstructionRepository.save(tomatoRecipeInstruction);
 
+        RecipeInstruction palakRecipeInstruction=new RecipeInstruction("Boil water and blanch washed spinach and let it coll and blend it and add all ingredients and spinach and paneer and cook it for 10 minutes");
+        recipeInstructionRepository.save(palakRecipeInstruction);
+
+
+
+        Recipe tomatoPulav=new Recipe("Tomato Pulav");
+        Recipe palakPaneer=new Recipe("Palak Paneer");
+//Adding all to recipe Tomato Pulav
+        tomatoPulav.addRecipeIngredients(tomatoPulavRecipeIngredient);
+        tomatoPulav.addRecipeIngredients(tomatoPulavRecipeIngredient2);
+        tomatoPulav.addRecipeIngredients(tomatoPulavRecipeIngredient3);
+        tomatoPulav.addRecipeIngredients(tomatoPulavRecipeIngredient4);
+
+        tomatoPulav.addRecipeInstruction(tomatoRecipeInstruction);
+        palakPaneer.addRecipeInstruction(palakRecipeInstruction);
+
+        tomatoPulav.addRecipeCategory(vegan);
+        palakPaneer.addRecipeCategory(vegitarian);
+
+        //PalakPaneer adding all to recipe
+        palakPaneer.addRecipeIngredients(palakPaneerIngredient1);
+        palakPaneer.addRecipeIngredients(palakPaneerIngredient2);
+        //   palakPaneer.addRecipeIngredients(palakPaneerIngredient3);
+
+
+        recipeRepository.save(tomatoPulav);
+        recipeRepository.save(palakPaneer);
     }
-
 
 
     @Test
@@ -120,7 +148,7 @@ class RecipeRepositoryTest {
         found.forEach(recipe -> System.out.println("hi "+recipe));
 
         assertEquals(1,found.size());
-      //  assertEquals(1, found.stream()
+        //  assertEquals(1, found.stream()
         //        .filter(recipe -> "Tomato Pulav".equalsIgnoreCase(recipe.getRecipeName())).count());
 
     }
@@ -130,15 +158,13 @@ class RecipeRepositoryTest {
 
         Set<Recipe> found=recipeRepository.findRecipesByCategoriesCategory("vegan");
         assertEquals(1,found.size());
-        found.stream().forEach(f -> System.out.println("hi rere"+f.getCategories()));
-        //assertEquals("Tomato Pulav",found.stream().anyMatch(recipe -> recipe.getCategories()));
         assertNotNull(found.stream().anyMatch(recipe -> "Tomato pulav".equalsIgnoreCase(recipe.getRecipeName())));
     }
 
     @Test
     void findAll(){
-     Collection<Recipe> resultFound = (Collection<Recipe>) recipeRepository.findAll();
-      assertEquals(1, resultFound.size());
+        Collection<Recipe> resultFound = (Collection<Recipe>) recipeRepository.findAll();
+        assertEquals(2, resultFound.size());
 
     }
 }
